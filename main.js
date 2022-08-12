f4a024e7d87e1fc20942eb3a577749321a4ad265
Bailey Cage <bailey.cage@gmail.com>
2022-08-12 12:30:23 -0400

    add functionality to heart icon

--- main.js
+++ main.js
@@ -2,7 +2,41 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// hide error modal
+const errorModal = document.getElementById('modal');
+errorModal.classList.add('hidden');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  // add event listener to heart icons
+  const likeGlyphColl = document.getElementsByClassName('like-glyph');
+  for (const likeGlyph of likeGlyphColl) {        // for each icon ...
+    likeGlyph.addEventListener('click', function(event) {
+      const heart = event.target;                 // get clicked-on icon
+      mimicServerCall()
+      .then(() => {
+        if (heart.textContent === EMPTY_HEART) {  // if empty, fill & activate
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
+        } else {                                  // if full, empty & de-activate
+          heart.textContent = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch((error) => {                       // if server error...
+        const errorMessage = errorModal.querySelector('#modal-message');
+        errorMessage.textContent = error;           // set error message
+        errorModal.classList.remove('hidden');      // display error modal
+        setTimeout(function() {                     // for 3 seconds, then reset
+          errorModal.classList.add('hidden');
+          errorMessage.textContent = '';
+        }, 3000);
+      });
+    });
+  }
+
+console.log(document.querySelector('.hidden'));
+
+});
 
 
 
